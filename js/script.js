6738940b8e78aaa61e6bb0ce3da1064941590820
cristian4699 <107372400+cristian4699@users.noreply.github.com>
2025-07-09 13:13:30 -0500

    First commit, basic web page layout

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let main_container = document.getElementById("main-container");
+let home_button = document.getElementById("home_button");
+let history_button = document.getElementById("history_button");
+let stadistics_button = document.getElementById("stadistics_button");
+let about_button = document.getElementById("about_button");
+let store_button = document.getElementById("store_button");
+let login_button = document.getElementById("login_button");
+
+
+home_button.addEventListener("click", function(){
+    alert("Boton home presionado");
+});
+
+history_button.addEventListener("click", function(){
+    alert("Boton history presionado");
+});
+
+stadistics_button.addEventListener("click", function(){
+    alert("Boton estadisticas presionado");
+});
+
+about_button.addEventListener("click", function(){
+    alert("Boton Sobre el proyecto presionado");
+});
+
+store_button.addEventListener("click", function(){
+    alert("Boton tienda virtual presionado");
+});
+
+login_button.addEventListener("click", function(){
+    alert("Boton login presionado");
+});
