a317859ac6490005948ef220af703c5f9350da41
cristian4699 <cristiandeveloper4699@gmail.com>
2025-07-30 22:47:56 -0500

    Modificacion en carga inicial del template landing

--- js/script.js
+++ js/script.js
@@ -34,22 +34,20 @@ function get_quote(){
     // Insertar el contenido clonado en el contenedor
     document.getElementById("main-container").appendChild(clon);
 }
-/* 
+ 
 window.addEventListener("load", (event) => {
-    let load_store = document.getElementById("load_button");
-    
-    load_store.addEventListener("click", function () {
+
+
     document.getElementById("main-container").innerHTML = "";
 
-    const plantilla = document.getElementById("store-section");
+    const plantilla = document.getElementById("landing-section");
 
     // Clonar el contenido de la plantilla
     const clon = plantilla.content.cloneNode(true);
 
     // Insertar el contenido clonado en el contenedor
     document.getElementById("main-container").appendChild(clon);
-    });
-}); */
+});
 
 
 home_button.addEventListener("click", function () {
@@ -90,37 +88,37 @@ stadistics_button.addEventListener("click", function () {
 
 /* Cargando formulario */
 
-document.getElementById("formularioEnergia").addEventListener("submit", function(e){
-    e.preventDefault();
+    document.getElementById("formularioEnergia").addEventListener("submit", function(e){
+        e.preventDefault();
 
-    let anio = parseInt(document.getElementById("anio").value);
-    let consumo = parseFloat(document.getElementById("consumoTotal").value);
+        let anio = parseInt(document.getElementById("anio").value);
+        let consumo = parseFloat(document.getElementById("consumoTotal").value);
 
-    // Usar fetch para obtener el JSON de Colombia
-    fetch('js/colombia.json')
-      .then(response => response.json())
-      .then(data => {
-        // Buscar el año ingresado
-        let registro = data.find(d => d.Year === anio);
+        // Usar fetch para obtener el JSON de Colombia
+        fetch('js/colombia.json')
+        .then(response => response.json())
+        .then(data => {
+            // Buscar el año ingresado
+            let registro = data.find(d => d.Year === anio);
 
-        if (registro) {
-          let porcentajeRenovable = registro["Renewables (% equivalent primary energy)"];
-          let consumoRenovable = (porcentajeRenovable / 100) * consumo;
+            if (registro) {
+            let porcentajeRenovable = registro["Renewables (% equivalent primary energy)"];
+            let consumoRenovable = (porcentajeRenovable / 100) * consumo;
 
-          let resultado = `
-            En Colombia en ${anio}, el ${porcentajeRenovable.toFixed(2)}% de la energía fue renovable.<br>
-            Si consumiste ${consumo.toFixed(2)} kWh, entonces ${consumoRenovable.toFixed(2)} kWh provienen de fuentes limpias.
-          `;
-          document.getElementById("resultado").innerHTML = resultado;
-        } else {
-          document.getElementById("resultado").innerHTML = `No hay datos para el año ${anio}.`;
-        }
-      })
-      .catch(error => {
-        console.error('Error al cargar los datos:', error);
-        document.getElementById("resultado").innerHTML = "Ocurrió un error al cargar los datos.";
-      });
-  });
+            let resultado = `
+                En Colombia en ${anio}, el ${porcentajeRenovable.toFixed(2)}% de la energía fue renovable.<br>
+                Si consumiste ${consumo.toFixed(2)} kWh, entonces ${consumoRenovable.toFixed(2)} kWh provienen de fuentes limpias.
+            `;
+            document.getElementById("resultado").innerHTML = resultado;
+            } else {
+            document.getElementById("resultado").innerHTML = `No hay datos para el año ${anio}.`;
+            }
+        })
+        .catch(error => {
+            console.error('Error al cargar los datos:', error);
+            document.getElementById("resultado").innerHTML = "Ocurrió un error al cargar los datos.";
+        });
+    });
 
 
 
@@ -229,7 +227,15 @@ store_button.addEventListener("click", function () {
 });
 
 login_button.addEventListener("click", function () {
-  alert("Boton login presionado");
+  document.getElementById("main-container").innerHTML = "";
+
+  const plantilla = document.getElementById("login-section");
+
+  // Clonar el contenido de la plantilla
+  const clon = plantilla.content.cloneNode(true);
+
+  // Insertar el contenido clonado en el contenedor
+  document.getElementById("main-container").appendChild(clon);
 });
 
 /* ----------------------  Manejo de templates ----------------*/
